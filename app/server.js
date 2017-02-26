c88d59a8089226586f51a33b5a003dfd32609468
Ashok Dey <ashokdey@users.noreply.github.com>
2017-02-26 16:37:53 +0530

    enabled CROS

--- app/server.js
+++ app/server.js
@@ -15,6 +15,27 @@ let port = process.env.PORT || 3000;
 // the body-parser middleware
 app.use(bodyParser.json());
 
+// Add headers for cros
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/_public/index.html');
 })
