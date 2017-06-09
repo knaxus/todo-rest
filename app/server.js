aab19c21e2a67400b25fdb61680482118d33bd39
Ashok Dey <ashokdey100@gmail.com>
2017-06-09 19:40:31 +0530

    install CORS Middleware

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ const config        = require('./config/config');
 const _             = require('lodash');
 const app           = require('express')();
 const bodyParser    = require('body-parser');
+const cors          = require('cors');
 
 const {ObjectID}        = require('mongodb');
 const {mongoose}        = require('./db/mongoose');
@@ -16,26 +17,7 @@ let port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-
-// Add headers for cros
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors())
 
 
 app.get('/', (req, res) => {
